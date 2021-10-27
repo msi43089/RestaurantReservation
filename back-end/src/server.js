5194630a4a6b0e8eea335cc7600b56bde4be3ae4
msi43089 <msi43089@gmail.com>
2021-10-26 22:31:10 -0400

    Deploy server test

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,4 @@
-const { PORT = 5000 } = process.env;
+/*const { PORT = 5000 } = process.env;
 
 const app = require("./app");
 const knex = require("./db/connection");
@@ -16,4 +16,23 @@ knex.migrate
 
 function listener() {
   console.log(`Listening on Port ${PORT}!`);
-}
+}*/
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const router = express.Router()
+const PORT = process.env.PORT || 5000
+
+router.get('/', cors(), (req, res) => {
+  res.json({ message: 'Hello Heroku!' });
+})
+
+app.use('/', router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} `);
+})
+
+module.exports = app
