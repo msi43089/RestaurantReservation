817b5c03d248df7d36bba3158415d5643f3c1731
msi43089 <msi43089@gmail.com>
2021-10-28 22:21:04 -0400

    Set state in res form. Migrate and seed res table

--- front-end/src/reservations/CreateReservation.js
+++ front-end/src/reservations/CreateReservation.js
@@ -1,40 +1,99 @@
-import React from "react"
+import React, {useState} from "react"
+import { useHistory } from "react-router"
+
+
+//goback not working properly
+
 
 function CreateReservation (){
+    const history = useHistory()
+    const initialState = {
+        firstName: "",
+        lastName: "",
+        mobileNumber: "",
+        date: "",
+        time: "",
+        size: ""
+    }
+    const [formData, setFormData ] = useState({...initialState})
 
+    function handleChange({target}){
+        setFormData({...formData,
+            [target.id]: target.value})
+    }
 
+    function submitHandler(){
+        history.push("/dashboard")
+    }
+    console.log(history)
 
     return (
         <>
         <h1>Create Reservation</h1>
         <form>
             <div className="form-group">
-                <label for="first-name">First Name</label>
-                <input input name="first_name" type="text" className="form-control" id="first-name" placeholder="Enter First Name" />
+                <label htmlFor="first-name">First Name</label>
+                <input  name="first_name"
+                        type="text" 
+                        className="form-control"
+                        id="firstName" 
+                        placeholder="Enter First Name"
+                        onChange={handleChange}
+                        value={formData.firstName} />
             </div>
             <div className="form-group">
-                <label for="last-name">Last Name</label>
-                <input name="last_name" type="text" className="form-control" id="last-name" placeholder="Enter Last Name" />
+                <label htmlFor="last-name">Last Name</label>
+                <input  name="last_name"
+                        type="text" 
+                        className="form-control" 
+                        id="lastName" 
+                        placeholder="Enter Last Name"
+                        onChange={handleChange}
+                        value={formData.lastName} />
             </div>
             <div className="form-group">
-                <label for="mobile-number">Mobile Number</label>
-                <input name="mobile_number" type="text" className="form-control" id="mobile-number" placeholder="Enter Mobile Phone Number" />
+                <label htmlFor="mobile-number">Mobile Number</label>
+                <input  name="mobile_number"
+                        type="text" 
+                        className="form-control" 
+                        id="mobileNumber" 
+                        placeholder="Enter Mobile Phone Number" 
+                        onChange={handleChange}
+                        value={formData.mobileNumber}/>
             </div>
             <div className="form-group">
-                <label for="reservation-date">Reservation Date</label>
-                <input name="reservation_date" type="text" className="form-control" id="reservation-date" placeholder="Enter Date" />
+                <label htmlFor="reservation-date">Reservation Date</label>
+                <input  name="reservation_date" 
+                        type="text" 
+                        className="form-control" 
+                        id="date" 
+                        placeholder="Enter Date"
+                        onChange={handleChange}
+                        value={formData.date} />
             </div>
             <div className="form-group">
-                <label for="reservation-time">Reservation Time</label>
-                <input name="reservation_time" type="text" className="form-control" id="reservation-time" placeholder="Enter Time" />
+                <label htmlFor="reservation-time">Reservation Time</label>
+                <input  name="reservation_time" 
+                        type="text" 
+                        className="form-control" 
+                        id="time" 
+                        placeholder="Enter Time"
+                        onChange={handleChange}
+                        value={formData.time} />
             </div>
             <div className="form-group">
-                <label for="people">Party Size</label>
-                <input name="people" type="text" className="form-control" id="people" placeholder="Enter Party Size" />
+                <label htmlFor="people">Party Size</label>
+                <input  name="people" 
+                        type="text" 
+                        className="form-control" 
+                        id="size" 
+                        placeholder="Enter Party Size"
+                        onChange={handleChange}
+                        value={formData.size} />
             </div>
             <div>
-                <button>Submit</button>
-                <button>Cancel</button>
+                <button onClick={submitHandler}>Submit</button>
+                <button onClick={() => history.goBack()}>Cancel</button>
             </div>
         </form>
         </>
